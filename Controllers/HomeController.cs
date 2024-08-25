2daaf6d363a40e5162227ffe2842fd4b7f19d264
LasandiRandini <randini-im20057@stu.kln.ac.lk>
2024-08-25 23:23:40 +0530

    finished

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,12 @@ namespace SpendSmart.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SpendSmartDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -20,11 +23,48 @@ namespace SpendSmart.Controllers
 
         public IActionResult Expenses()
         {
+            var allExpenses = _context.Expenses.ToList();
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense( int? id)
+        {
+            if(id !=null)
+            {
+                //editing --> load an expense by id
+                var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+                return View(expenseInDb);
+            }
             return View();
         }
+        public IActionResult DeleteExpense(int id)
+        {
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense=> expense.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses"); 
+        }
+
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            if(model.Id == 0)
+            {
+                //Create
+                _context.Expenses.Add(model);
+
+            } else
+            {
+                //Editing
+                _context.Expenses.Update(model);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
